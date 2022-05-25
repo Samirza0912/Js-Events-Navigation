58b33e8900553aaa2051d5c43b2b4b2dcd6eddb9
Samirza0912 <100156117+Samirza0912@users.noreply.github.com>
2022-05-25 17:55:43 +0400

    Add files via upload

--- movingdiv/assets/script.js
+++ movingdiv/assets/script.js
@@ -1,8 +1,31 @@
-let move=document.getElementById("circle");
+let move=document.querySelector(".circle");
+let step=10;
+window.addEventListener('load',()=>{
+    move.style.position='absolute';
+    move.style.left=0;
+    move.style.top=0;
+})
 
 // move.addEventListener("keypress",function(event){
 //     console.log(event);
 // });
-document.onkeyup=function(e){
-    console.log("salam");
-}
+// document.onkeyup=function(e){
+//     console.log("salam");
+// }
+
+window.addEventListener('keydown',(e)=>{
+    switch (e.key) {
+        case 'ArrowLeft':
+            move.style.left=parseInt(move.style.left)-step+"px";
+            break;
+        case 'ArrowRight':
+            move.style.left=parseInt(move.style.left)+step+"px";
+            break;
+        case 'ArrowUp':
+            move.style.top=parseInt(move.style.top)-step+"px";
+            break;
+        case 'ArrowDown':
+            move.style.top=parseInt(move.style.top)+step+"px";
+            break;
+    }
+})
