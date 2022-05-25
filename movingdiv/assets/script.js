fab76180d029ef2e83d79286a1020481b1a84b3d
Samirza0912 <100156117+Samirza0912@users.noreply.github.com>
2022-05-25 05:29:27 +0400

    "feat:still working on it"

---
+++ movingdiv/assets/script.js
@@ -0,0 +1,8 @@
+let move=document.getElementById("circle");
+
+// move.addEventListener("keypress",function(event){
+//     console.log(event);
+// });
+document.onkeyup=function(e){
+    console.log("salam");
+}
